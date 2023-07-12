3f5013f082618a7ae573f68bd2a8c401faea2c25
Vinícius Santos <102265413+dosantosv@users.noreply.github.com>
2023-07-12 17:56:04 -0300

    Add files via upload

---
+++ Calculator/Calculator.v2/Calculator/Media.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Calculator
+{
+    internal class Media
+    {
+        static public void CalcularMedia()
+        {
+            try
+            {
+                double nota;
+                double notas;
+                double soma = 0;
+
+                Console.Write("Informe quantas notas serão calculadas por favor: ");
+                int qtdeNotas = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                for (notas = 1; notas <= qtdeNotas; notas++)
+                {
+                    Console.WriteLine($"Digite a nota {notas}");
+                    while (!double.TryParse(Console.ReadLine(), out nota))
+                    {
+                        Console.WriteLine($"O valor deve ser um número!");
+                    }
+                    soma += nota;
+                }
+
+                var media = soma / qtdeNotas;
+                ImprimeResultado(media);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Código executado inadequadamente! " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+        static public void ImprimeResultado(double media)
+        {
+            if (media >= 6)
+            {
+                Console.WriteLine("Você passou de série.");
+            }
+            else
+            {
+                Console.WriteLine("Você foi reprovado.");
+            }
+            Console.WriteLine($"A sua média foi {media}");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Calculator/Calculator.v2/Calculator/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Somar();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("------------------");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtraçao");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Calcular Média");
+            Console.WriteLine("0 - SAIR");
+            Console.WriteLine("------------------");
+
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            int res;
+
+            if (int.TryParse(key.KeyChar.ToString(), out res))
+            {
+                switch (res)
+                {
+                    case 1: Soma(); break;
+                    case 2: Subtracao(); break;
+                    case 3: Divisao(); break;
+                    case 4: Multiplicacao(); break;
+                    case 5: Media.CalcularMedia(); break;
+                    case 0: System.Environment.Exit(0); break;
+                    default: Menu(); break;
+                }
+            } else
+            {
+                Console.WriteLine("Opção inválida! Digite um número válido.");
+                Console.ReadKey();
+                Menu();
+            }
+
+        }
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Somar()
+        {
+            try
+            {
+                double numero;
+                double numeros;
+                double soma = 0;
+
+                Console.Write("Informe quantos número serão somados por favor: ");
+                double qtdeNotas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                for (numeros = 1; numeros <= qtdeNotas; numeros++)
+                {
+                    Console.WriteLine($"Digite o valor {numeros}");
+                    while (!double.TryParse(Console.ReadLine(), out numero))
+                    {
+                        Console.WriteLine($"O valor deve ser um número!");
+                    }
+                    soma += numero;
+                }
+
+                var numerosSomados = soma;
+                Console.WriteLine($"O valor somado é: {numerosSomados}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Código executado inadequadamente! " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
