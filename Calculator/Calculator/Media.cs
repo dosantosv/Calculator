2c1d4bc680d18f3d9c35a37fc68ecc99509efda8
Vinícius Santos <102265413+dosantosv@users.noreply.github.com>
2023-07-11 22:19:56 -0300

    Add files via upload

---
+++ Calculator/Calculator/Media.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Media
+    {
+        static public void VerificarValor()
+        {
+            try
+            {
+                double nota;
+                double notas;
+                double soma = 0;
+                for (notas = 1; notas <= 4; notas++)
+                {
+                    Console.WriteLine($"Digite a nota {notas}");
+                    while (!double.TryParse(Console.ReadLine(), out nota))
+                    {
+                        Console.WriteLine($"O valor deve ser um número!");
+                    }
+                    soma += nota;
+                }
+                var media = CalcularMedia(soma);
+                ImprimeResultado(media);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Código executado inadequadamente! " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        static public double CalcularMedia(double soma)
+        {
+            double media = soma / 4;
+
+            return media;
+        }
+
+        static public void ImprimeResultado(double media)
+        {
+            if (media >= 6)
+            {
+                Console.WriteLine("Você passou de série.");
+            }
+            else
+            {
+                Console.WriteLine("Você foi reprovado.");
+            }
+            Console.WriteLine($"A sua média foi {media}");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("------------------");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtraçao");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Calcular Média");
+            Console.WriteLine("0 - SAIR");
+            Console.WriteLine("------------------");
+
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            int res;
+
+            if (int.TryParse(key.KeyChar.ToString(), out res))
+            {
+                switch (res)
+                {
+                    case 1: Soma(); break;
+                    case 2: Subtracao(); break;
+                    case 3: Divisao(); break;
+                    case 4: Multiplicacao(); break;
+                    case 5: CalcularMedia(); break;
+                    case 0: System.Environment.Exit(0); break;
+                    default: Menu(); break;
+                }
+            } else
+            {
+                Console.WriteLine("Opção inválida! Digite um número válido.");
+                Console.ReadKey();
+                Menu();
+            }
+
+        }
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void CalcularMedia()
+        {
+            Console.Clear();
+
+            double[] notas = new double[4];
+
+            for (int v = 1; v < notas.Length; v++)
+            {
+                Console.WriteLine($"DIGITE O VALOR DA NOTA {v}");
+                notas[v] = double.Parse(Console.ReadLine());
+            }
+            double soma = notas.Sum();
+            Console.WriteLine("");
+            double media = soma / 3;
+            Console.WriteLine($"A média total das 3 notas é: {media}");
+
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
